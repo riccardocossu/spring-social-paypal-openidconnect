8d3af4036bd39ffb00927debd9514c13d40e7e71
Riccardo Cossu <riccardo.cossu@gmail.com>
2014-06-04 17:08:45 +0200

    added support for sandbox environment

--- src/main/java/org/springframework/social/openidconnect/PayPalConnectionProperties.java
+++ src/main/java/org/springframework/social/openidconnect/PayPalConnectionProperties.java
@@ -9,7 +9,8 @@ import org.springframework.util.Assert;
 
 /**
  * <p>
- * Properties for connecting PayPal Access. Used to give default values for PayPal Access.
+ * Properties for connecting PayPal Access. Used to give default values for
+ * PayPal Access.
  * </p>
  */
 public final class PayPalConnectionProperties {
@@ -17,12 +18,15 @@ public final class PayPalConnectionProperties {
     /**
      * Logger for the class.
      */
-    private static Logger logger = Logger.getLogger(PayPalConnectionProperties.class);
+    private static Logger logger = Logger
+	    .getLogger(PayPalConnectionProperties.class);
 
     /**
      * Properties Holder.
      */
     private static final Properties PROPS = new Properties();
+
+    private static String keyPrefix = "";
 
     /**
      * Properties should be accessed in static way.
@@ -32,27 +36,35 @@ public final class PayPalConnectionProperties {
     }
 
     /**
-     * Loads properties file based on environment. A default one is included in the jar which is named as
-     * connection-prod.properties. If you wish to override, and not use these properties, you can do that while creating
+     * Loads properties file based on environment. A default one is included in
+     * the jar which is named as connection-prod.properties. If you wish to
+     * override, and not use these properties, you can do that while creating
      * connection factory
      * 
      */
     static {
-        if (PROPS.isEmpty()) {
-            try {
-                ClassPathResource resource = new ClassPathResource("connection-prod.properties");
-                PROPS.load(resource.getInputStream());
-                logger.debug("default properties loaded");
-                Assert.notNull(PROPS.get("authorizeEndpoint"), "Authorize endpoint should be specified");
-                Assert.notNull(PROPS.get("tokenEndpoint"), "Token endpoint should be specified");
-                Assert.notNull(PROPS.get("checkidEndpoint"), "Check Id Endpoint should be specified");
-                Assert.notNull(PROPS.get("userinfoEndpoint"), "User Info endpoint should be specified");
-                Assert.notNull(PROPS.get("disconnectEndpoint"), "Disconnect endpoint should be specified");
-            } catch (IOException e) {
-                logger.error("properties file not found...you have to override url values.");
-                throw new RuntimeException("connection-prod properties file not found");
-            }
-        }
+	if (PROPS.isEmpty()) {
+	    try {
+		ClassPathResource resource = new ClassPathResource(
+			"connection-prod.properties");
+		PROPS.load(resource.getInputStream());
+		logger.debug("default properties loaded");
+		Assert.notNull(PROPS.get("authorizeEndpoint"),
+			"Authorize endpoint should be specified");
+		Assert.notNull(PROPS.get("tokenEndpoint"),
+			"Token endpoint should be specified");
+		Assert.notNull(PROPS.get("checkidEndpoint"),
+			"Check Id Endpoint should be specified");
+		Assert.notNull(PROPS.get("userinfoEndpoint"),
+			"User Info endpoint should be specified");
+		Assert.notNull(PROPS.get("disconnectEndpoint"),
+			"Disconnect endpoint should be specified");
+	    } catch (IOException e) {
+		logger.error("properties file not found...you have to override url values.");
+		throw new RuntimeException(
+			"connection-prod properties file not found");
+	    }
+	}
     }
 
     /**
@@ -61,7 +73,7 @@ public final class PayPalConnectionProperties {
      * @return - Authorize endpoint uri
      */
     public static String getAuthorizeEndpoint() {
-        return (String) PROPS.get("authorizeEndpoint");
+	return PROPS.getProperty(keyPrefix + "authorizeEndpoint");
     }
 
     /**
@@ -70,7 +82,7 @@ public final class PayPalConnectionProperties {
      * @return - Token endpoint uri
      */
     public static String getTokenEndpoint() {
-        return (String) PROPS.get("tokenEndpoint");
+	return PROPS.getProperty(keyPrefix + "tokenEndpoint");
     }
 
     /**
@@ -79,7 +91,7 @@ public final class PayPalConnectionProperties {
      * @return - CheckId endpoint uri
      */
     public static String getCheckIdEndpoint() {
-        return (String) PROPS.get("checkidEndpoint");
+	return PROPS.getProperty(keyPrefix + "checkidEndpoint");
     }
 
     /**
@@ -88,7 +100,7 @@ public final class PayPalConnectionProperties {
      * @return - Userinfo endpoint uri
      */
     public static String getUserInfoEndpoint() {
-        return (String) PROPS.get("userinfoEndpoint");
+	return PROPS.getProperty(keyPrefix + "userinfoEndpoint");
     }
 
     /**
@@ -97,7 +109,11 @@ public final class PayPalConnectionProperties {
      * @return - Disconnect endpoint uri
      */
     public static String getDisconnectEndpoint() {
-        return (String) PROPS.get("disconnectEndpoint");
+	return PROPS.getProperty(keyPrefix + "disconnectEndpoint");
+    }
+
+    public static void initSandbox() {
+	keyPrefix = "sandbox.";
     }
 
 }
